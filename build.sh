8bdf48765bc7fdbad6d8ca3937e8322238d89478
verysync <2578842696@qq.com>
2018-09-06 15:55:41 +0800

    build: 修复冲突

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ if [[ "$PACKAGE_VERSION" == "" ]]; then
     exit 1
 fi
 
-<<<<<<< HEAD
+
 gsed -i "s/^PACKAGE_VERSION=.*/PACKAGE_VERSION=$PACKAGE_VERSION/" verysync/install.sh
-=======
-gsed -i 's/^PACKAGE_VERSION=.*/PACKAGE_VERSION=$PACKAGE_VERSION/' install.sh
->>>>>>> 25f736630d5db3490001003b633850f32faba6b6
 tar czvf dist/verysync-v$PACKAGE_VERSION.tar.gz verysync
